f6dceb6ac3c046237b51160fa4b57f8f3d8d6b5f
euflauzinoandre <78576825+euflauzinoandre@users.noreply.github.com>
2026-09-01 21:21:05 +0100

    create operator function and attribute to equal button

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,11 +1,15 @@
-const display = document.querySelector("#display");
+const display = document.querySelector("#display-value");
 
 const buttons = document.querySelector("#keyboard");
 const digits = buttons.querySelectorAll(".digits");
 
+let firstNumber = "";
+
 digits.forEach((element) =>
 	element.addEventListener("click", () => {
-		display.textContent += element.textContent;
+		firstNumber += element.textContent;
+
+		display.textContent = value;
 	}),
 );
 
@@ -18,3 +22,20 @@ const backspace = document.querySelector("#backspace");
 backspace.addEventListener("click", () => {
 	display.textContent = display.textContent.slice(0, -1);
 });
+
+const equal = document.querySelector("equal");
+equal.addEventListener("click", () => {
+	operate(firstNumber, secondNumber, operator);
+});
+
+function operate(firstNumber, secondNumber, operator) {
+	return operator == "+"
+		? firstNumber + secondNumber
+		: operator == "–"
+			? firstNumber - secondNumber
+			: operator == "x"
+				? firstNumber * secondNumber
+				: operator == "÷"
+					? firstNumber / secondNumber
+					: null;
+}
