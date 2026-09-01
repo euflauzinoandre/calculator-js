569bbe9bd55ef4ef622362fe25917a5e7c8a40a9
euflauzinoandre <78576825+euflauzinoandre@users.noreply.github.com>
2026-09-02 00:56:40 +0100

    create getOperator and the logic to first and second number and function equal to calc and show result

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -4,33 +4,51 @@ const buttons = document.querySelector("#keyboard");
 const digits = buttons.querySelectorAll(".digits");
 
 let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+let value = "";
 
 digits.forEach((element) =>
 	element.addEventListener("click", () => {
-		firstNumber += element.textContent;
-
-		display.textContent = value;
+		value += element.textContent;
+		display.textContent += value;
 	}),
 );
 
+//Clear all data display
 const allClear = document.querySelector("#allClear");
 allClear.addEventListener("click", () => {
 	display.textContent = "";
+	value = "";
 });
 
+//Clear the last display caractere
 const backspace = document.querySelector("#backspace");
 backspace.addEventListener("click", () => {
 	display.textContent = display.textContent.slice(0, -1);
 });
 
-const equal = document.querySelector("equal");
+//Get any operator
+const getOperator = document.querySelectorAll(".operators");
+getOperator.forEach((element) =>
+	element.addEventListener("click", () => {
+		firstNumber = value;
+		operator = element.textContent;
+		display.textContent = firstNumber + operator;
+		value = "";
+	}),
+);
+
+const equal = document.querySelector("#equal");
 equal.addEventListener("click", () => {
-	operate(firstNumber, secondNumber, operator);
+	secondNumber = value;
+	display.textContent = String(operate(firstNumber, secondNumber, operator));
+	value = "";
 });
 
 function operate(firstNumber, secondNumber, operator) {
 	return operator == "+"
-		? firstNumber + secondNumber
+		? Number(firstNumber) + Number(secondNumber)
 		: operator == "–"
 			? firstNumber - secondNumber
 			: operator == "x"
